8872dbd3d2a482430095fc3fdf7ce3cc281d23db
Alpha Olomi <alphaolomi@gmail.com>
2020-10-24 00:07:45 +0300

    minor update and clean up

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
-# Face-Detection
-This is a simple program in python that can detect faces in images
+# Face Detection using OpenCV
+
+A simple program in python that can detect faces in images
+
+## Requirements
+
+- OpenCV
+
+## Getting started
+
+See https://dev.to/adityagamedev/face-recognition-in-python-5b1o for detailed explanations 
+
+Clone
+
+```sh
+# Virtualenv modules installation (Windows based systems)
+# virtualenv venv
+# .\venv\Scripts\activate.bat
+# Virtualenv modules installation (Unix based systems)
+virtualenv venv
+source venv/bin/activate
+
+# Install requirements
+# use pip and python in Windows
+pip3 install opencv-python
+
+# 
+python3 main.py
+```
+
+##  License
+
+This project is MIT licensed. 
 
 
-Just Clone it and you are done!
-
-## Requirements :
-OpenCV 
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from cv2 import cv2
 
-img = cv2.imread('man.png') # must pass valid file directory
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #converting the image into grayscale image
+# Must pass valid file directory
+img = cv2.imread('images/man.png') 
 
+# Converting the image into grayscale image
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
 
-
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
-
+face_cascade = cv2.CascadeClassifier("classifiers/haarcascade_frontalface_default.xml")
 
 faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.05, minNeighbors=5)
 
@@ -15,7 +14,10 @@ for x, y, w, h in faces:
     img = cv2.rectangle(img, (x,y), (x+w,y+h), (255, 255, 250),3)
 
 
+# Displaying the image
+cv2.imshow("ImageWindow", img) 
 
-cv2.imshow("ImageWindow", img) # displaying the image
-cv2.waitKey(0) #waitKey is set to 0, that means the image window will close as soon as any key is pressed.
-cv2.destroyAllWindows() 
+# WaitKey is set to 0, 
+# that means the image window will close as soon as any key is pressed.
+cv2.waitKey(0)
+cv2.destroyAllWindows()
